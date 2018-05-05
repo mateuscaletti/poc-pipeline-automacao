762d4da61656d45ba25f07c9208e49d47515cd36
Mateus Caletti <mateus@caletti.com.br>
2018-05-05 14:48:58 -0300

    Jenkins

--- jenkins/scripts-linux/startProjects.sh
+++ jenkins/scripts-linux/startProjects.sh
@@ -3,9 +3,17 @@
 WORKSPACE=$1
 BUILD_NUMBER=$2
 
+echo "Inicializando os projetos..."
+
 for f in $WORKSPACE/projects/*; do
-        SCRIPT_DEFINE="$f/startProject.sh"
-        if [ -d ${f} ] && [ -e ${SCRIPT_DEFINE} ]; then
-                echo $f
-        fi
+	echo "Inicializando os projetos..."
+	
+    SCRIPT_DEFINE="$f/startProject.sh"
+	
+	
+    if [ -d ${f} ] && [ -e ${SCRIPT_DEFINE} ]; then
+		echo "Projeto '$SCRIPT_DEFINE' inicializando..."
+		sh $SCRIPT_DEFINE
+		echo "Projeto '$SCRIPT_DEFINE' finalizado!"
+    fi
 done
