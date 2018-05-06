dc5b60ada15cfe57b791d19a6b31490ce90e9f94
Mateus Caletti <mateus@caletti.com.br>
2018-05-06 19:08:04 -0300

    Jenkins Windows+Linux stage start

--- jenkins/scripts-windows/startProjects.bat
+++ jenkins/scripts-windows/startProjects.bat
@@ -16,11 +16,13 @@ for /d %%A in ("%WORKSPACE%"\projects\%STAGE_NAME%\*) do (
 	
 	echo Verificando se existe arquivo '!SCRIPT_DEFINE!' ...
 	if exist !SCRIPT_DEFINE! (
+		SET RESULT_DIR=%WORKSPACE%\results\instance_%BUILD_NUMBER%\%STAGE_NAME%\!PROJECT_NAME!
+		
 		echo Projeto !PROJECT_NAME! inicializando...
-		echo Criando diretório dos resultados do projeto...
-		mkdir "%WORKSPACE%\results\instance_%BUILD_NUMBER%\%STAGE_NAME%\!PROJECT_NAME!"
+		echo Criando diretório dos resultados do projeto em "!RESULT_DIR!"...
+		mkdir "!RESULT_DIR!"
 		echo Executando o projeto
-		call !SCRIPT_DEFINE! "%WORKSPACE%" %BUILD_NUMBER% >> "%WORKSPACE%\results\instance_%BUILD_NUMBER%\%STAGE_NAME%\!PROJECT_NAME!\execution_%STAGE_NAME%_!PROJECT_NAME!.log"
+		call !SCRIPT_DEFINE! "%WORKSPACE%" %BUILD_NUMBER% >> "!RESULT_DIR!\execution_%STAGE_NAME%_!PROJECT_NAME!.log"
 		echo Projeto '!PROJECT_NAME!' finalizado!
 	)
 )
